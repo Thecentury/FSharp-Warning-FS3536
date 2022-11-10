6e3e4c0503c0cbcb0e4cbb852451462c385f20c5
Mikhail Brinchuk <mikhail.brinchuk@prodrive-technologies.com>
2022-11-10 20:56:38 +0100

    Added a remark about the unused but required open directive

--- Project/Prelude.fs
+++ Project/Prelude.fs
@@ -1,8 +1,7 @@
 ﻿module Prelude
 
+// This open directive is crucial to produce the FS3536 warning, though no code from that namespace is actually used.
 open FSharp.Core.Fluent
-
-(*--------------------------------------------------------------------------------------------------------------------*)
 
 // Function produces a warning
 // Prelude.fs(8, 18): [FS3536] 'IEnumerable<_>' is normally used as a type constraint in generic code, e.g. "'T when ISomeInterface<'T>" or "let f (x: #ISomeInterface<_>)". See https://aka.ms/fsharp-iwsams for guidance. You can disable this warning by using '#nowarn "3536"' or '--nowarn:3536'.
