3b2290084deebbccef4e3fdbb88451f548bf6a97
Mikhail Brinchuk <mikhail.brinchuk@prodrive-technologies.com>
2022-11-10 20:53:44 +0100

    Created a project reproducing the FS3536 warning

---
+++ Project/Prelude.fs
@@ -0,0 +1,9 @@
+﻿module Prelude
+
+open FSharp.Core.Fluent
+
+(*--------------------------------------------------------------------------------------------------------------------*)
+
+// Function produces a warning
+// Prelude.fs(8, 18): [FS3536] 'IEnumerable<_>' is normally used as a type constraint in generic code, e.g. "'T when ISomeInterface<'T>" or "let f (x: #ISomeInterface<_>)". See https://aka.ms/fsharp-iwsams for guidance. You can disable this warning by using '#nowarn "3536"' or '--nowarn:3536'.
+let listAsSeq (l : List<'a>) = l :> seq<'a>
